7803172cb0f3b74573d43612315b3a9b5bc8fe18
DESKTOP-PMQOTRO\Николай <dmitriyrostov2016@gmail.com>
2023-04-17 21:26:32 +0300

    commit

--- src/main/java/CoordinatesDTO.java
+++ src/main/java/CoordinatesDTO.java
@@ -1,2 +1,26 @@
-public class CoordinateDTO {
+import com.github.javafaker.Faker;
+
+public class CoordinatesDTO {
+
+ private Float  lon;  // longitude
+
+ private Float  lat;  // latitude
+
+
+ public CoordinatesDTO(Float lon, Float lat){
+    this.lon = lon;
+    this.lat = lat;
+
+ }
+
+ public CoordinatesDTO() {
+    final Faker faker = new Faker;
+
+    this.lon = Float.valueOf(faker.address().longitude());
+    this.lat = Float.valueOf(faker.address().latitude());
+
+ }
+
+
+
 }
